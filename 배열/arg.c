c83fb5fa36078db50bafc78ba201e5645bd1786d
yoon-dbsrude <77610266+yoon-dbsrude@users.noreply.github.com>
2021-01-25 15:15:12 +0900

    주석 추가

--- 배열/arg.c
+++ 배열/arg.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc==2)
+    if(argc==2)             // 추가 입력하는 것은 index 1부터~
     {
         printf("hello, %s\n", argv[1]);
     }
@@ -12,3 +12,11 @@ int main(int argc, char* argv[])
         printf("hello, world\n");a
     }
 }
+
+
+/* 입력  
+>>> ./arg.exe yoon             // argc[0]=./arg.exe ,  argc[1]=Yoon
+hello, yoon
+>>> ./arg.exe
+hello, world
+*/
